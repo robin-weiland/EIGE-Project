190f97ba0d56dfa76133e38edf01e4d801a0038c
ge93zam <ge93zam@mytum.de>
2020-01-10 17:57:17 +0100

    Added Base player commands

---
+++ EIGE-Project/Assets/Scripts/PlayerCommands/PlayerMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : PlayerCommand
+{
+    public void run(PlayerManager player)
+    {
+        player.rigidbody2D.velocity = new Vector2(player.properties.movementSpeed * Input.GetAxis("Horizontal"), player.rigidbody2D.velocity.y);
+    }
+}
---
+++ EIGE-Project/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private List<PlayerCommand> fixedCommands = new List<PlayerCommand>();
+    private List<PlayerCommand> commands = new List<PlayerCommand>();
+    [SerializeField]
+    public PlayerProperties properties = new PlayerProperties();
+    public Rigidbody2D rigidbody2D { private set; get; }
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.freezeRotation = true;
+        registerCommand(new PlayerMove());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (PlayerCommand command in fixedCommands)
+        {
+            command.run(this);
+        }
+    }
+
+    private void registerCommand(PlayerCommand command, bool isFixed = true)
+    {
+        if (isFixed) fixedCommands.Add(command);
+        else commands.Add(command);
+    }
+}
