91c216dcb1e939fac02cb09b9927e9c0b90be99a
Daniel Klitzner <ge75lod@mytum.de>
2020-01-11 16:29:05 +0100

    Added Basic Jump

--- EIGE-Project/Assets/Scripts/PlayerCommands/PlayerJump.cs
+++ EIGE-Project/Assets/Scripts/PlayerCommands/PlayerJump.cs
@@ -2,12 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerJump : PlayerCommand
+public class PlayerJump : PlayerDualCommand
 {
+    private float jumpTimeCounter;
+    private bool grounded;
+    private bool isJumping;
+
     public void run(PlayerManager player)
     {
-        if (Input.GetButtonDown("Jump")) {
-            player.GetComponent<Rigidbody2D>().velocity = Vector2.up * player.properties.jumpVelocity;
+        grounded = Physics2D.OverlapCircle(player.properties.feetPosition.position, player.properties.isGroundedCircleRadius, player.properties.isGround);
+
+        if (grounded) {
+            jumpTimeCounter = player.properties.maxJumpTime;
         }
+    }
+
+    public void fixedRun(PlayerManager player)
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                player.rigidbody2D.velocity = new Vector2(player.rigidbody2D.velocity.x, player.properties.jumpVelocity);
+                isJumping = true;
+            }
+        }
+
+        if (Input.GetButton("Jump") && isJumping) { 
+            if(jumpTimeCounter > 0) {
+                player.rigidbody2D.velocity = new Vector2(player.rigidbody2D.velocity.x, player.properties.jumpVelocity);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump")) {
+            isJumping = false;
+            jumpTimeCounter = 0;
+        }
+
+
+
     }
 }
--- EIGE-Project/Assets/Scripts/PlayerProperties.cs
+++ EIGE-Project/Assets/Scripts/PlayerProperties.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerProperties
 {
+    //Move
     [SerializeField]
     public float movementSpeed;
+
+    //Jump
+    public Transform feetPosition;
+
+    public float isGroundedCircleRadius;
+
     [Range(1, 10)]
     public float jumpVelocity;
+
+    public float maxJumpTime;
+
+    public LayerMask isGround;
+
+
 
 }
