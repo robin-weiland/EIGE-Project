7c095df177c7569266725e70680a6534c31d7c3f
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2020-01-12 00:37:51 +0100

    Added Stone Tile

--- EIGE-Project/Assets/Scripts/PlayerCommands/PlayerMove.cs
+++ EIGE-Project/Assets/Scripts/PlayerCommands/PlayerMove.cs
@@ -7,20 +7,15 @@ public class PlayerMove : PlayerCommand
     private bool facingLeft = true;
     public override void run(PlayerManager player)
     {
-       player.rigidbody2D.velocity = new Vector2(player.properties.movementSpeed * Input.GetAxis("Horizontal"), player.rigidbody2D.velocity.y);
-       if(player.rigidbody2D.velocity.x != 0)
-        {
-            player.GetComponent<Animator>().SetInteger("State", 1);
-        }
-        else
-        {
-            player.GetComponent<Animator>().SetInteger("State", 0);
-        }
+        player.rigidbody2D.velocity = new Vector2(player.properties.movementSpeed * Input.GetAxis("Horizontal"), player.rigidbody2D.velocity.y);
+
+        //Animations
+        player.GetComponent<Animator>().SetFloat("Speed", Mathf.Abs(player.rigidbody2D.velocity.x)*10);
         if (Input.GetAxis("Horizontal") > 0 && facingLeft == false){
             facingLeft = true;
             player.rigidbody2D.transform.Rotate(0, 180, 0);
-       }
-       else if (Input.GetAxis("Horizontal") < 0&& facingLeft == true) {
+        }
+        else if (Input.GetAxis("Horizontal") < 0&& facingLeft == true) {
             facingLeft = false;
             player.rigidbody2D.transform.Rotate(0, 180, 0);
         }
