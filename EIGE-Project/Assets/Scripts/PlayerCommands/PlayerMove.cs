084d0b72fc895c39489ace14cf5543b1318c3987
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2020-01-11 17:41:25 +0100

    Player Movement controls Player Animation - WIP

--- EIGE-Project/Assets/Scripts/PlayerCommands/PlayerMove.cs
+++ EIGE-Project/Assets/Scripts/PlayerCommands/PlayerMove.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class PlayerMove : PlayerCommand
 {
+    private bool facingLeft = true;
     public void run(PlayerManager player)
     {
-        player.rigidbody2D.velocity = new Vector2(player.properties.movementSpeed * Input.GetAxis("Horizontal"), player.rigidbody2D.velocity.y);
+       player.rigidbody2D.velocity = new Vector2(player.properties.movementSpeed * Input.GetAxis("Horizontal"), player.rigidbody2D.velocity.y);
+       if(player.rigidbody2D.velocity.x != 0)
+        {
+            player.GetComponent<Animator>().SetInteger("State", 1);
+        }
+       if (Input.GetAxis("Horizontal") > 0 && facingLeft == false){
+            facingLeft = true;
+            player.rigidbody2D.transform.Rotate(0, 180, 0);
+       }
+       else if (Input.GetAxis("Horizontal") < 0&& facingLeft == true) {
+            facingLeft = false;
+            player.rigidbody2D.transform.Rotate(0, 180, 0);
+        }
     }
 }
