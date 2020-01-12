f7a9538e8bf387afb01618ba768bada91b884f58
Daniel Klitzner <ge75lod@mytum.de>
2020-01-12 03:38:17 +0100

    Added Camera Follow Skript

---
+++ EIGE-Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    void Update()
+    {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2, -2);
+        
+    }
+}
