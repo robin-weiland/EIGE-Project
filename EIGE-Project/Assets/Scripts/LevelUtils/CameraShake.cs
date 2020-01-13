37c588fbc913f87f87bb9bf89f391dfa2e972510
Robin 'r0w' Weiland <robinweiland@gmx.de>
2020-01-13 14:04:57 +0100

    added CameraShake, DistanceMeasurement and some gizmos

---
+++ EIGE-Project/Assets/Scripts/LevelUtils/CameraShake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CameraShake : MonoBehaviour
+{
+    private static CameraShake _instance;
+    private Vector3 _originalPos;
+    private float _timeAtCurrentFrame;
+    private float _timeAtLastFrame;
+    private float _fakeDelta;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Update() {
+        // Calculate a fake delta time, so we can Shake while game is paused.
+        _timeAtCurrentFrame = Time.realtimeSinceStartup;
+        _fakeDelta = _timeAtCurrentFrame - _timeAtLastFrame;
+        _timeAtLastFrame = _timeAtCurrentFrame; 
+    }
+
+    public static void Shake (float duration, float amount) {
+        _instance._originalPos = _instance.gameObject.transform.localPosition;
+        _instance.StopAllCoroutines();
+        _instance.StartCoroutine(_instance._shake(duration, amount));
+    }
+
+    private IEnumerator _shake (float duration, float amount) {
+        while (duration > 0) {
+            transform.localPosition = _originalPos + Random.insideUnitSphere * amount;
+            duration -= _fakeDelta;
+            yield return null;
+        }
+        transform.localPosition = _originalPos;
+    }
+}
---
+++ EIGE-Project/Assets/Scripts/LevelUtils/DistanceMeasurement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceMeasurement : MonoBehaviour
+{
+    private float _startEndDistance;
+    public GameObject start, end;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _startEndDistance = Vector2.Distance(start.transform.position, end.transform.position);
+    }
+    
+    public static float Get(GameObject start, GameObject end)
+    {
+        return Vector2.Distance(start.transform.position, end.transform.position);
+    }
+
+    public float Get()
+    {
+        return _startEndDistance;
+    }
+}
