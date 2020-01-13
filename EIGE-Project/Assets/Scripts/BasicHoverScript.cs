1dba759ed1ddba16ce927b439c761bbb5fc632b0
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2020-01-13 17:49:25 +0100

    Added Platform Prefab, Cliff Prefab, Hover Script

---
+++ EIGE-Project/Assets/Scripts/BasicHoverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicHoverScript: MonoBehaviour
+{
+    public float HoverStrength;
+    public float HoverSpeed;
+    float yposition;
+    void Start()
+    {
+        yposition = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector2(this.transform.position.x, yposition + (Mathf.Cos(Time.time * HoverSpeed) * HoverStrength));
+    }
+}
