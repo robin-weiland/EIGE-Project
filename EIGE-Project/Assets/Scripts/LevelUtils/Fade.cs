764f32d325ce925a7d801f389c9cbf01786d3955
Robin 'r0w' Weiland <robinweiland@gmx.de>
2020-01-15 14:03:06 +0100

    added Fade meta file

---
+++ EIGE-Project/Assets/Scripts/LevelUtils/Fade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    [Header("SceneFade")]
+    [SerializeField] 
+    public Image fader;
+    
+    [SerializeField] [Range(1f, 10f)]
+    public float fadeInLength;
+    
+    [SerializeField] [Range(1f, 10f)]
+    public float fadeOutLength;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.GetType());
+        fader.canvasRenderer.SetAlpha(1f);
+        fader.CrossFadeAlpha(0f, fadeInLength, false);
+    }
+
+    private void OnDestroy()
+    {
+        fader.CrossFadeAlpha(1f, fadeOutLength, false);
+        fader.canvasRenderer.SetAlpha(1f);
+    }
+}
