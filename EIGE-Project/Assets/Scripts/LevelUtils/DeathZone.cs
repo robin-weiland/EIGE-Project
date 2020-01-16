acb39c69c1ebf10b8e906f0f293dc9744aee1b48
Franz Mörike <ge38was@mytum.de>
2020-01-16 17:30:26 +0100

    added Deathzone & Franz Sandbox

---
+++ EIGE-Project/Assets/Scripts/LevelUtils/DeathZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    public GameObject player; 
+
+    public void Update()
+    {
+        this.transform.position = new Vector2(player.transform.position.x, -10f);
+    }
+    public void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.tag == "Player")
+        {
+            Debug.Log("Dead");
+            //Destroy(player.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
