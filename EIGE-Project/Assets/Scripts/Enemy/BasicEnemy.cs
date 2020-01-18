fedd639c55e4d526389e136fbbb8a7655cac1900
Robin 'r0w' Weiland <robinweiland@gmx.de>
2020-01-18 22:46:01 +0100

    basic enemy (not finished)

---
+++ EIGE-Project/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public GameObject player, patrol1, patrol2;
+    private DistanceMeasurement playerDistance, patrol1Distance, patrol2Distance;
+
+    private int gotoPatrol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gotoPatrol = 2;
+        
+        playerDistance = gameObject.AddComponent<DistanceMeasurement>();
+        playerDistance.other = player;
+
+        patrol1Distance = gameObject.AddComponent<DistanceMeasurement>();
+        patrol1Distance.other = patrol1;
+
+        patrol2Distance = gameObject.AddComponent<DistanceMeasurement>();
+        patrol2Distance.other = patrol2;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (gotoPatrol)
+        {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+}
