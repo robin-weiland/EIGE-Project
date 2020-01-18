c1479de2bf6584b0bd02175d37651249a25db672
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 23:07:57 +0100

    Added not finished mechanic and changed ground friction and linear drag

---
+++ EIGE-Project/Assets/Scripts/Orbs/Mechanic/PullMechanic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullMechanic : OrbMechanic
+{
+    public PullMechanic()
+    {
+
+    }
+
+    public override void onPickup(PlayerManager player)
+    {
+
+    }
+
+    public override void holdingFixedUpdate(PlayerManager player)
+    {
+
+    }
+
+    public override void holdingUpdate(PlayerManager player)
+    {
+
+    }
+
+    public override void onDrop(PlayerManager player)
+    {
+
+    }
+}
