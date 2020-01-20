3072d48f35f11cb0522ff44408ec1a3055dbb1e1
Robin 'r0w' Weiland <robinweiland@gmx.de>
2020-01-20 23:02:47 +0100

    added Level enum into scenemanager

--- EIGE-Project/Assets/Scripts/GeneralScripts/Scenemanager.cs
+++ EIGE-Project/Assets/Scripts/GeneralScripts/Scenemanager.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum Level
+{
+    Intro,
+    // TODO add levels here and below
+}
+
 public class Scenemanager : MonoBehaviour
 {
+    public Level previous, next;
+
+    public string GetLevel(Level lvl)
+    {
+        switch (lvl)
+        {
+            case Level.Intro: return "Scenes/Intro";
+            // TODO add levels here and above
+            default: throw new Exception("Couldn't resolve level!");
+        }
+    }
+    
     public Scene returnCurrentScene()
     {
         return SceneManager.GetActiveScene();
@@ -15,11 +33,26 @@ public class Scenemanager : MonoBehaviour
     }
     public void switchSceneToLevel(int level)   //do level -1
     {
-        StartCoroutine(switchSceneToLevelAsynch(level));
+        StartCoroutine(switchSceneToLevelAsync(level));
     }
-    IEnumerator switchSceneToLevelAsynch(int level)
+    IEnumerator switchSceneToLevelAsync(int level)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+        //TODO Franz
+    }
+    
+    public void switchSceneToLevel(Level level)   //do level -1
+    {
+        StartCoroutine(switchSceneToLevelAsync(level));
+    }
+    IEnumerator switchSceneToLevelAsync(Level level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(GetLevel(level));
         while (!operation.isDone)
         {
             Debug.Log(operation.progress);
