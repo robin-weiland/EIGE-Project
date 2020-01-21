8dca3ce3cabf24d7c61ee2084046db83b01dd032
BuildTools <unconfigured@null.spigotmc.org>
2020-01-21 16:59:39 +0100

    Pull now goes with movement of ohter objects. And shift as additional key

--- EIGE-Project/Assets/Scripts/Orbs/Mechanic/PullMechanic.cs
+++ EIGE-Project/Assets/Scripts/Orbs/Mechanic/PullMechanic.cs
@@ -7,6 +7,8 @@ public class PullMechanic : OrbMechanic
     private GameObject hook;
     private GameObject current;
 
+    private int cur = 0, cooldown = 20;
+
     public PullMechanic(GameObject hook)
     {
         this.hook = hook;
@@ -14,18 +16,22 @@ public class PullMechanic : OrbMechanic
 
     public override void holdingUpdate(PlayerManager player)
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (current == null && (Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftShift)) && cur <= 0)
         {
             Vector2 direction = Input.mousePosition;
             direction = Camera.main.ScreenToWorldPoint(direction);
             direction = (direction - (Vector2)player.transform.position).normalized;
             current = Object.Instantiate(hook, player.transform.position, Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x / 2, direction.y / 2) * Mathf.Rad2Deg));
             current.GetComponent<HookBehavior>().origin = player;
+            cur = cooldown;
         }
-        if (current != null && !Input.GetKey(KeyCode.LeftShift))
+        if (current != null && !(Input.GetMouseButton(0) || Input.GetKey(KeyCode.LeftShift)))
         {
             Object.Destroy(current);
+            current = null;
         }
+        
+        if (cur > 0) cur--;
     }
 
     public override void onPickup(PlayerManager player)
