813ad87fe92b2e578b8fa0de56fe797368aadcda
Daniel Klitzner <ge75lod@mytum.de>
2020-01-26 12:46:54 +0100

    Changed Star Behaviour

--- EIGE-Project/Assets/Scripts/LevelUtils/StarsScrolling.cs
+++ EIGE-Project/Assets/Scripts/LevelUtils/StarsScrolling.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class StarsScrolling : MonoBehaviour
 {
     [SerializeField]
-    float scrollSpeed = 0.08f; 
+    float scrollSpeed = 0.08f;
+    float originalX;
+
+    public void Start()
+    {
+        originalX = this.transform.position.x;
+    }
     public void Update()
     {
         float amtToMove = scrollSpeed * Time.deltaTime;
         this.transform.Translate(amtToMove * Vector3.right, Space.World);
 
-        if (this.transform.position.x > -4)
+        if (this.transform.position.x < originalX - 4)
         {
-            this.transform.position = new Vector3(-48f, this.transform.position.y, this.transform.position.z);
+            this.transform.position = new Vector3(originalX, this.transform.position.y, this.transform.position.z);
         }
     }
 }
