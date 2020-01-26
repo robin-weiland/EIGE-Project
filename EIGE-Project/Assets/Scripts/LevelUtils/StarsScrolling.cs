3d8c063bd35b45e7d9787d2e36f92e88a8d6dac7
Pydes-boop <59734957+Pydes-boop@users.noreply.github.com>
2020-01-26 12:38:14 +0100

    Added Level 1... again

--- EIGE-Project/Assets/Scripts/LevelUtils/StarsScrolling.cs
+++ EIGE-Project/Assets/Scripts/LevelUtils/StarsScrolling.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class StarsScrolling : MonoBehaviour
 {
     [SerializeField]
-    float scrollSpeed = 0.08f;
+    float scrollSpeed = 0.08f; 
     public void Update()
     {
         float amtToMove = scrollSpeed * Time.deltaTime;
-        transform.Translate(amtToMove * Vector3.right, Space.World);
+        this.transform.Translate(amtToMove * Vector3.right, Space.World);
 
-        if (transform.position.x > -4)
+        if (this.transform.position.x > -4)
         {
-            transform.position = new Vector3(-48f, transform.position.y, transform.position.z);
+            this.transform.position = new Vector3(-48f, this.transform.position.y, this.transform.position.z);
         }
     }
 }
