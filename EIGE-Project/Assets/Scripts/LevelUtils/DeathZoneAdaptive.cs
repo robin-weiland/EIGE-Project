5f202d46fe96f9b954a59537e44cf5b8d1949db0
ge93zam <ge93zam@mytum.de>
2020-01-29 22:49:19 +0100

    Added Gravity Level

---
+++ EIGE-Project/Assets/Scripts/LevelUtils/DeathZoneAdaptive.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZoneAdaptive : MonoBehaviour
+{
+    [SerializeField]
+    private float posX = 0;
+    [SerializeField]
+    private float posY = 0;
+    [SerializeField]
+    private GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector2(
+            posX == 0 ? player.transform.position.x : posX,
+            posY == 0 ? player.transform.position.y : posY);
+    }
+
+    public void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.tag == "Player")
+        {
+            // Reset Gravity
+            Physics2D.gravity = new Vector2(0, -9.8f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //try other version using own scenemanager
+        }
+    }
+}
