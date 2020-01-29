06b00f511573e579b76e678100cc98f871b9f2dc
Franz Mörike <ge38was@mytum.de>
2020-01-29 19:14:01 +0100

    added clouds to other scenes; minor fixes

--- EIGE-Project/Assets/Scripts/LevelUtils/CloudsScrolling.cs
+++ EIGE-Project/Assets/Scripts/LevelUtils/CloudsScrolling.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class CloudsScrolling : MonoBehaviour
 {
-    [SerializeField]
-    float scrollSpeed = 2f;
+    private float scrollSpeed = 0.8f;
     void Update()
     {
         float amtToMove = scrollSpeed * Time.deltaTime;
         transform.Translate(amtToMove * Vector3.right, Space.World);
 
-        if (transform.position.x > 46f)
+        if (transform.position.x > 50f)
         {
             transform.position = new Vector3(-155f, transform.position.y, transform.position.z);
         }
