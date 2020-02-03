ac68f4feb9050a144d681b4446b5e982d9d87b6d
ge93zam <ge93zam@mytum.de>
2020-02-03 23:58:33 +0100

    Added new timer UI

---
+++ EIGE-Project/Assets/Scripts/UI/UIOrbFading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOrbFading : MonoBehaviour
+{
+    [SerializeField]
+    private Image fading;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mathf.Sin(Time.time) / 4;
+        Color w = Color.white;
+        w.a = Mathf.Sin(Time.time * 2) / 4 + 0.75f;
+        fading.color = w;
+    }
+}
