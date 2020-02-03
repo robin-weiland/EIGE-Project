053833c0d996ef0c8677a1aa335ddf898d6644f0
ge93zam <ge93zam@mytum.de>
2020-02-03 20:11:02 +0100

    Moved Files added UI

---
+++ EIGE-Project/Assets/Scripts/UI/OrbRefs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class OrbRef
+{
+    public OrbType type;
+    public Sprite bar;
+    public Sprite up;
+    public Sprite orb;
+}
